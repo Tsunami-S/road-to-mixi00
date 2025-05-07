226a1d1cbb35b503427585fb9ae0bdf6dfe80359
Tsunami Saito <tssaito@c6r3s9.42tokyo.jp>
2025-05-07 15:01:05 +0900

    fix dsn

--- minimal_sns_app/app/test/setup_db.go
+++ minimal_sns_app/app/test/setup_db.go
@@ -3,6 +3,7 @@ package test
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
 	"log"
 	"time"
 	"minimal_sns_app/db"
@@ -10,7 +11,7 @@ import (
 )
 
 func InitTestDB() *gorm.DB {
-	dsn := "root:@tcp(db:3306)/app?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := os.Getenv("MYSQL_DSN")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("❌ DB接続失敗: %v", err)
--- minimal_sns_app_bonus/app/test/setup_db.go
+++ minimal_sns_app_bonus/app/test/setup_db.go
@@ -3,12 +3,13 @@ package test
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
 	"log"
 	"time"
 )
 
 func initTestDB() *gorm.DB {
-	dsn := "root:@tcp(db:3306)/app?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := os.Getenv("MYSQL_DSN")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("❌ DB接続失敗: %v", err)
